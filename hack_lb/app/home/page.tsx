0b082d7b6607e4070066b4defd40248e6fbb1cd7
adrian-szwejk <aszwe2@illinois.edu>
2023-07-04 08:50:06 -0500

    added Hack Logo and styling for githublink

--- hack_lb/app/home/page.tsx
+++ hack_lb/app/home/page.tsx
@@ -3,17 +3,22 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
+
 export default function Home() {
-    const apiUrl = 'https://api.hackillinois.org/profile/leaderboard/?limit=10'
-  // HackIllinois API Link https://api.hackillinois.org/profile/leaderboard/
+    const apiUrl = 'https://api.hackillinois.org/profile/leaderboard/'
+    //I could just set the limit to 10, 25, or 50 but I wanted to make it more dynamic (not sure how)
+    const apiUrlLimit10 = 'https://api.hackillinois.org/profile/leaderboard/?limit=10'
+    const apiUrlLimit25 = 'https://api.hackillinois.org/profile/leaderboard/?limit=25'
+    const apiUrlLimit50 = 'https://api.hackillinois.org/profile/leaderboard/?limit=50'
     const [users, setUsers] = useState([]);
     let displayData
     function getData() {
       fetch(apiUrl).then((response) => response.json()).then((responseData) => {
         
-        displayData = responseData['profiles'].map(function(user:any){
+        displayData = responseData['profiles'].map(function(user:any, index:any){
           return (
-            <p key = {user.id}>{user.discord}</p>
+            <p key = {user.id} className="border border-slate-300 text-slate-300 px-2 py-1 mt-4 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">{index+1}. {user.discord} {user.points}</p>
+            
           )
         })
         console.log(responseData)
@@ -27,10 +32,10 @@ export default function Home() {
   //You put the array after the useEffect function to make it run one "first mount" or when it first launches
   return (
     <div>
-      <h1 className = "text-4xl">Leaderboard</h1>
-      <Link href="https://github.com/adrian-szwejk/hackIllinois_leaderboard_react">Link to Github Repo </Link>
-      <h3 className = "text-2xl">Top 10 Users</h3>
+      <h1 className = "text-4xl mb-4">Leaderboard</h1>
+      <Link href="https://github.com/adrian-szwejk/hackIllinois_leaderboard_react" className="border border-slate-200 text-slate-200 px-2 py-1 mb-4 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">Link to Github Repo </Link>
       
+      <h3 className = "text-2xl mt-4">Top Users</h3>
       {users}
     </div>
   );
--- hack_lb/app/page.tsx
+++ hack_lb/app/page.tsx
@@ -4,6 +4,7 @@ export default function Onboard() {
   return (
     <>
       <header className="flex justify-between items-center mb-4">
+      <img alt="Logo" src = "../_next/static/media/hack_img.184d4684.png" style={{ height: "70px", paddingRight: "10px" }}/> 
         <h1 className="text-2xl">Leaderboard Website for HackIllinois</h1>
         <Link
           className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
