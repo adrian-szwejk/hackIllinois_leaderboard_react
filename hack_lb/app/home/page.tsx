aadf34e92f5a50bd5e6357638e7ca9921085d3d5
adrian-szwejk <aszwe2@illinois.edu>
2023-07-04 08:21:51 -0500

    api showing data on screen working

--- hack_lb/app/home/page.tsx
+++ hack_lb/app/home/page.tsx
@@ -3,17 +3,25 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-export default async function Home() {
-  const apiUrl = 'https://jsonplaceholder.typicode.com/users'
+export default function Home() {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/todos'
   // HackIllinois API Link https://api.hackillinois.org/profile/leaderboard/
     const [users, setUsers] = useState([]);
-  useEffect(() => {
-    const getData = async () => {
-        const query = await fetch(apiUrl);
-        const response = await query.json();
-        console.log('response from API: ',response);
-        setUsers(response);
+    let displayData
+    function getData() {
+      fetch(apiUrl).then((response) => response.json()).then((responseData) => {
+        
+        displayData = responseData.map(function(user:any){
+          return (
+            <p key = {user.id}>{user.title}</p>
+          )
+        })
+        console.log(responseData)
+        setUsers(displayData)
+      })  
     }
+  useEffect(() => {
+    
     getData();
   },[])
   //You put the array after the function to make it run one "first mount" or when it first launches
@@ -22,21 +30,8 @@ export default async function Home() {
       <h1 className = "text-4xl">Leaderboard</h1>
       <Link href="https://github.com/adrian-szwejk/hackIllinois_leaderboard_react">Link to Github Repo </Link>
       <h3 className = "text-2xl">Top 10 Users</h3>
-      {
-        users && users.length && users.map((users:any, index) => {
-          return (
-            <li key={index}>{users.name}</li>
-          )
-            // return (
-            //     <>
-            //     {/* <h2 className = "text-xl">{users['profiles'][index]['discord']} {users['profiles'][index]['points']}</h2> */}
-            //     <h4>
-            //         index {users.name}
-            //     </h4>
-            //     </>
-            // )
-        })
-      }
+      
+      {users}
     </div>
   );
 }
