c019751142b78e225b97fec4522f7d3d7e81275f
adrian-szwejk <aszwe2@illinois.edu>
2023-07-04 01:49:24 -0500

    styled and made links between screens/github work

---
+++ hack_lb/app/home/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className = "text-4xl">Leaderboard</h1>
+      <Link href="https://github.com/adrian-szwejk/hackIllinois_leaderboard_react">Link to Github Repo </Link>
+      
+    </div>
+  );
+}
--- hack_lb/app/page.tsx
+++ hack_lb/app/page.tsx
@@ -1,5 +1,17 @@
-export default function Page() {
+import Link from "next/link";
+
+export default function Onboard() {
   return (
-    <div>hi</div>
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Leaderboard Website for HackIllinois</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/home">
+          Continue
+        </Link>
+      </header>
+      
+    </>
   );
 }
