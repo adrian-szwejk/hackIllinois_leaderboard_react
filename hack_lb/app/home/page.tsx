25374f904ab03ac58f7a027757232dc41861e168
adrian-szwejk <aszwe2@illinois.edu>
2023-07-04 02:21:20 -0500

    logging api information but throwing error

--- hack_lb/app/home/page.tsx
+++ hack_lb/app/home/page.tsx
@@ -1,11 +1,37 @@
-import Link from "next/link";
+"use client"; // This is a client component 👈🏽
 
-export default function Home() {
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default async function Home() {
+    const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+        // HackIllinois API Link https://api.hackillinois.org/profile/leaderboard/
+        const query = await fetch('https://jsonplaceholder.typicode.com/users');
+        const response = await query.json();
+        console.log('response from API: ',response);
+        setUsers(response);
+    }
+    getData();
+  },[])
   return (
     <div>
       <h1 className = "text-4xl">Leaderboard</h1>
       <Link href="https://github.com/adrian-szwejk/hackIllinois_leaderboard_react">Link to Github Repo </Link>
-      
+      <h3 className = "text-2xl">Top 10 Users</h3>
+      {
+        users && users.length && users.map((users:any) => {
+            return (
+                <>
+                {/* <h2 className = "text-xl">{users['profiles'][index]['discord']} {users['profiles'][index]['points']}</h2> */}
+                <h4>
+                    {users.name}
+                </h4>
+                </>
+            )
+        })
+      }
     </div>
   );
 }
